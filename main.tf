7445d98cf44393b0f0387f4ab2488fa8d40d0f2a
siddharth sharma <sharmasid2398@gmail.com>
2021-05-08 11:07:12 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
    features {}
 }
 # Reference to the current subscription.  Used when creating role assignments
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "siddharth_rg" {
   name     = "siddharth_rg"
   location = "East US"
 }
