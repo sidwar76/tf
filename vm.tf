0d1d5f3c81e22b590098308e75db3cdea5c0a83f
siddharth sharma <sharmasid2398@gmail.com>
2021-05-10 08:04:09 +0000

    Updated vm.tf

--- vm.tf
+++ vm.tf
@@ -1,22 +1,22 @@
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "siddharth_rg" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.siddharth_rg.location
+  resource_group_name = azurerm_resource_group.siddharth_rg.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.siddharth_rg.name
+  virtual_network_name = azurerm_virtual_network.siddharth_rg.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "rg" {
   count = 2
   name                = "${count.index}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.siddharth_rg.location
+  resource_group_name = azurerm_resource_group.siddharth_rg.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -28,8 +28,8 @@ resource "azurerm_network_interface" "rg" {
 resource "azurerm_virtual_machine" "main" {
   count = 2
   name                  = "${count.index}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.siddharth_rg.location
+  resource_group_name   = azurerm_resource_group.siddharth_rg.name
    network_interface_ids = [
      azurerm_network_interface.rg.*.id[count.index],
    ]
