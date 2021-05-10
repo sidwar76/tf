d7d612eba21f62cb3ba8207c36480754cd86d6b4
siddharth sharma <sharmasid2398@gmail.com>
2021-05-10 07:57:40 +0000

    Added import.tf

---
+++ import.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+    
+    features {}
+}
+# This will be specific to your own Terraform State in Azure storage
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "siddhart_sg"
+    storage_account_name  = "sidwar1"
+    container_name        = "sid"
+    key                   = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "example" {}
