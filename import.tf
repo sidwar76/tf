1104d0c35d5e6a3559c7c16823f81ae89d03dfb2
siddharth sharma <sharmasid2398@gmail.com>
2021-05-10 08:34:59 +0000

    Updated import.tf

--- import.tf
+++ import.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
-    
-    features {}
+  subscription_id = "0d3ce63c-abaa-48ae-bbe1-f582cea576b9"
+  client_id       = "811398c3-7846-4b90-84b9-2c4585a28aaa" 
+  client_secret   = "3d8YqB.G3.Q~eCfhLcx.nZXr_79m384st."
+  tenant_id       = "e4e34038-ea1f-4882-b6e8-ccd776459ca0"
+  features {}
 }
 # This will be specific to your own Terraform State in Azure storage
 terraform {
