b6e64501f8f4c0c7b19e603e4deff00083f114c8
siddharth sharma <sharmasid2398@gmail.com>
2021-05-10 07:43:08 +0000

    Updated sql.tf

--- sql.tf
+++ sql.tf
@@ -1,16 +1,16 @@
 
 resource "azurerm_storage_account" "example" {
   name                     = "${var.name}sstorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = siddharth_rg
+  location                 = "${var.location}"
   account_tier             = "${var.plan_tier}"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_sql_server" "example" {
   name                         = "${var.name}-mysqlserver"
-  resource_group_name          = azurerm_resource_group.example.name
-  location                     = azurerm_resource_group.example.location
+  resource_group_name          = siddharth_rg
+  location                     = "${var.location}"
   version                      = "12.0"
   administrator_login          = "${var.loginid}"
   administrator_login_password = "${var.password}"
