bfafafacbf9f000b362490f07eadf03ad8041f41
siddharth sharma <sharmasid2398@gmail.com>
2021-05-10 08:09:39 +0000

    Updated acr.tf

--- acr.tf
+++ acr.tf
@@ -4,8 +4,8 @@ locals {
 
 resource "azurerm_container_registry" "default" {
   name                     = "${local.acr_name}"
-  resource_group_name      = "${azurerm_resource_group.example.name}"
-  location                 = "${azurerm_resource_group.example.location}"
+  resource_group_name      = "${azurerm_resource_group.siddharth_rg.name}"
+  location                 = "${azurerm_resource_group.siddharth_rg.location}"
   sku                      = "Standard"
   admin_enabled            = false
 }
